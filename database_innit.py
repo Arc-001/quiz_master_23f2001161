53dfb9b3b722aab526ef5cbfd9463607523f2aa3
Arc-001 <25131347+Arc-001@users.noreply.github.com>
2025-01-28 01:11:05 +0530

    Fixed the model :)

--- database_innit.py
+++ database_innit.py
@@ -13,14 +13,17 @@ class Subject(Base):
     subject_id = Column(Integer, primary_key=True)
     name = Column(String)
     description = Column(String)
+    Chapters = relationship("Chapter", back_populates="subject", cascade = "all, delete, delete-orphan")
 
 
 class Chapter(Base):
     __tablename__="chapter"
     chapter_id = Column(Integer, primary_key=True)
-    Subject_id = Column(Integer, ForeignKey('subject.subject_id'))
+    Subject_id = Column(Integer,ForeignKey('subject.subject_id'),nullable= False )
     name = Column(String, nullable=False)
     description = Column(String)
+    subject = relationship("Subject", back_populates="Chapters")
+    quizes = relationship("Quiz", back_populates="chapter", cascade = "all, delete, delete-orphan")
 
 class Quiz(Base):
     __tablename__="quiz"
@@ -31,6 +34,8 @@ class Quiz(Base):
     date_of_quiz = Column(DateTime, default = datetime.utcnow)
     time_duration = Column(String, default = "NA")
     remarks = Column(String, default = "NA")
+    chapter = relationship("Chapter", back_populates="quizes")
+    subject  = relationship("Subject", back_populates="Chapters")
     question = relationship("Question", back_populates="quiz", cascade = "all, delete, delete-orphan")
 
 class Question(Base):
@@ -39,21 +44,17 @@ class Question(Base):
     quiz_id = Column(Integer, ForeignKey('quiz.quiz_id'))
     question_stmt = Column(String, nullable = False)
     quiz = relationship("Quiz", back_populates="question")
+    chapter = relationship("Chapter", back_populates="quizes")
+    subject = relationship("Subject", back_populates="Chapters")
     option = relationship("belongs_to", back_populates="question", cascade = "all, delete, delete-orphan")
-
-class belongs_to(Base):
-    __tablename__ = "belongs_to"
-    question_id = Column(Integer, ForeignKey('question.question_id'), primary_key = True)
-    option_id = Column(Integer, ForeignKey('option.option_id'), primary_key = True)
-    question = relationship("Question", back_populates="belongs_to")
-    option = relationship("Option", back_populates="belongs_to")
-
 
 class Option(Base):
     __tablename__="option"
-    option_id = Column(Integer,primary_key= True, nullable = False)
+    option_id = Column(Integer,primary_key= True,  autoincrement= True)
+    question_id = Column(Integer, ForeignKey('question.question_id'), nullable = False)
     option_text = Column(String, nullable = False)
     is_correct = Column(Boolean, nullable = False, default = False)
+    question = relationship("Question", back_populates="option")
 
 class User(Base):
     __tablename__ = "user"
@@ -69,22 +70,25 @@ class User(Base):
 class Score(Base):
     __tablename__ = "score"
     score_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey="user.user_id")
-    quiz_id = Column(Integer, ForeignKey="quiz.quiz_id")
-    attempt_id= Column(Integer, ForeignKey="attempt.attempt_id")
+    user_id = Column(Integer, ForeignKey("user.user_id"),nullable=False)
+    quiz_id = Column(Integer, ForeignKey("quiz.quiz_id"), nullable=False)
+    attempt_id= Column(Integer, ForeignKey("attempt.attempt_id"), nullable=False)
     attempt_date_time = Column(DateTime)
     total_score = Column(Integer)
     user = relationship("User", back_populates="score")
+    quiz = relationship("Quiz", back_populates="score")
     attempt = relationship("Attempt", back_populates="score", cascade = "all, delete, delete-orphan")
 
 class Attempt(Base):
     __tablename__ = "attempt"
+    user_id = Column(Integer, ForeignKey("user.user_id"))
     attempt_id = Column(Integer, primary_key=True)
-    score_id = Column(Integer, ForeignKey = "score.score_id")
+    score_id = Column(Integer, ForeignKey("score.score_id"))
     correct = Column(Integer)
     wrong_question_csv = Column(String)
     total_question = Column(Integer)
     score = relationship("Score", back_populates="attempt")
+    user = relationship("User", back_populates="attempt")
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
