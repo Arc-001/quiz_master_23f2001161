761839e3550341c62611af81808c40ae5938d24f
Arc <25131347+Arc-001@users.noreply.github.com>
2025-03-24 22:00:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,26 +2,28 @@
 
 file_name="quizmaster_23f2001161"
 
+echo "======================= Checking for Python ======================="
+
 if ! command -v python3 &> /dev/null
 then
     echo "Python3 is not installed."
     exit 1
 fi
 
-echo "Creating venv for the app..."
+echo "=======================Creating venv======================="
 python3 -m venv "./$file_name"
 
-echo "Activating the virtual environment..."
+echo "=======================Activating the virtual environment======================="
 source ".$file_name/bin/activate"
 
 
-echo "Installing dependencies from requirements.txt..."
+echo "=======================Installing dependencies======================="
 pip install -r requirements.txt
 
 
-echo "Setup complete!!"
-echo "to start the server run python3 app.py"
-echo "Starting for the first time"
+echo "=======================Setup complete======================="
+echo "to start the server run ./quizmaster_23f2001161/bin/python3 app.py"
+echo "=======================Starting for the first time======================="
 
 python3 app.py
 
